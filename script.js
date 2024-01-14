bcc925dd82a4c6cd7452b423aef61cd609edacc4
majazimnoch <120421674+majazimnoch@users.noreply.github.com>
2024-01-14 21:15:33 +0100

    removed one argument from observer function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const observer = new IntersectionObserver((entries, observer) => {
+const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         console.log(entry)
         if (entry.isIntersecting) {
